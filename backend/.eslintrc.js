37c509ada16394717e52b298025896f2eb823c7d
xxx <1104132275@qq.com>
2019-03-13 22:18:41 +0800

    upload sql (old version)

---
+++ backend/.eslintrc.js
@@ -0,0 +1,32 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "node": true,
+//         "es6": true
+//     },
+//     "extends": "eslint:recommended",
+//     "parserOptions": {
+//         "ecmaVersion": 5
+//     },
+//     "rules": {
+//         "indent": [
+//             "error",
+//             "tab"
+//         ],
+//         "linebreak-style": [
+//             "error",
+//             "windows"
+//         ],
+//         "quotes": [
+//             "error",
+//             "double"
+//         ],
+//         "semi": [
+//             "error",
+//             "always"
+//         ]
+//     },
+//     "parserOptions": {
+//         "sourceType": "module"
+//     }
+// };
